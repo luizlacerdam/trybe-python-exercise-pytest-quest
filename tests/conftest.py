db1b716737c79532fe944260183468262fae4bcb
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-07 15:41:24 -0400

    :sparkles: [feat] parametrized_test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def custom_fixture():
     return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- tests/parametrized_test.py
+++ tests/parametrized_test.py
@@ -6,6 +6,17 @@ from src.hex_converter import hexadecimal_to_decimal  # noqa: F401
 pytestmark = pytest.mark.dependency  # NÃO REMOVA ESSA LINHA
 
 
-def test_converter():
-    assert hexadecimal_to_decimal("1") == 1
-    assert hexadecimal_to_decimal("2") == 2
+@pytest.mark.parametrize(
+    "input_string, expected_output",
+    [
+        ("8", 8),
+        ("9", 9),
+        ("a", 10),
+        ("b", 11),
+        ("c", 12),
+        ("e", 14),
+        ("f", 15),
+    ],
+)
+def test_converter(input_string, expected_output):
+    assert hexadecimal_to_decimal(input_string) == expected_output
