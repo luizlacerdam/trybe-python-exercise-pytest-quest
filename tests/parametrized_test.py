b2d0d36ce55a2f40f07c6ef16e0c69134dd276d7
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-07 15:29:40 -0400

    :sparkles: [feat] parametrized_test

--- tests/parametrized_test.py
+++ tests/parametrized_test.py
@@ -4,3 +4,8 @@ from src.hex_converter import hexadecimal_to_decimal  # noqa: F401
 
 # aplica o marcador de dependency para todos os testes do arquivo
 pytestmark = pytest.mark.dependency  # NÃO REMOVA ESSA LINHA
+
+
+def test_converter():
+    assert hexadecimal_to_decimal("1") == 1
+    assert hexadecimal_to_decimal("2") == 2
